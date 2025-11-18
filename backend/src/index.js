6c4ec8e2bc37895f9c9c07f83e8c6d63e3a9e5ef
Nexila <info@nexilatechnologies.com>
2025-11-18 12:46:30 +0530

    full code

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,19 @@
-console.log("Backend Works")
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// connect DB
+connectDB(process.env.MONGO_URI);
+
+// routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/leads", require("./routes/leads"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running on port: - index.js:19", PORT));
